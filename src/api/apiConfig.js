6bc20bff3cdd47cae5967e9e2bed692e71d33208
P-Ange <92918485+P-Ange@users.noreply.github.com>
2021-12-30 14:50:17 +0100

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,7 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
     apiKey: 'bf828bf28a95bddeffa13893d9c94e3c',
+    language: 'fr',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
